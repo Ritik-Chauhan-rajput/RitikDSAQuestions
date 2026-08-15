39baf1b3d0e75ed98bd99a14ba63651a9320310a
Ritik Chauhan <varunc723@gmail.com>
2026-08-16 01:00:07 +0530

    Time: 4 ms (99.07%), Space: 47.7 MB (48.19%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean palindromeHelper(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;   // Corrected
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return palindromeHelper(i + 1, j, s)
+                        || palindromeHelper(i, j - 1, s);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
