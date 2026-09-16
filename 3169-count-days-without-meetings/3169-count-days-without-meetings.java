871d60c8c411c232bc056223b48490cb0b854531
Ritik Chauhan <varunc723@gmail.com>
2026-09-16 22:42:33 +0530

    Time: 41 ms (34.02%), Space: 124.2 MB (11.4%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,26 +1,23 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(a->a[0]));
-        List<int[]> res=new ArrayList<>();
-        res.add(meetings[0]);
-        for(int i=1;i<meetings.length;i++){
-            // meetings[i].start< res.get(res.size()-1).end
-            // merge
-            // else: meeting -> push it in the result array
-            if(meetings[i][0]<=res.get(res.size()-1)[1]){
-                res.get(res.size()-1)[0]=Math.min(res.get(res.size()-1)[0],meetings[i][0]);
-                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],meetings[i][1]);
-            }
-            else{
-                res.add(meetings[i]);
-            }
-        }
+    //    res..
+    //    res.add(meeting[0])
+    //    meeting... start> merged_intervals(res last)
+    //    maxEnd=meetings[0][1]
+    //    from next mering onward....
+    //    current meeting... start
+        int maxEnd=meetings[0][1];
         int gap=0;
-        for(int i=1;i<res.size(); i++){
-            gap=gap+(res.get(i)[0]-res.get(i-1)[1])-1;
+        for(int i=1;i<meetings.length;i++){
+            if(meetings[i][0]>maxEnd){
+                gap=gap+meetings[i][0]-maxEnd-1;
+
+            }
+            maxEnd=Math.max(maxEnd, meetings[i][1]);
         }
-        gap=gap+res.get(0)[0]-1;
-        gap=gap+days-res.get(res.size()-1)[1];
+        gap=gap+meetings[0][0]-1;
+        gap=gap+days-maxEnd;
         return gap;
     }
 }
