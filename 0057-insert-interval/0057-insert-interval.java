77efcd36be34a4339d92688d840bb9193cff9061
Ritik Chauhan <varunc723@gmail.com>
2026-09-21 11:11:59 +0530

    Time: 0 ms (100%), Space: 47.2 MB (37.59%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,38 +1,39 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-
-        List<int[]> res = new ArrayList<>();
-
-        int i = 0;
-        int n = intervals.length;
-
-        // 1. Intervals completely before newInterval
-        while (i < n && intervals[i][1] < newInterval[0]) {
+        // insert position.... keep inserting intervals in the res
+        // while overlaping.... update interval
+        // no overlapping.... insert newinterval into res
+        // insert remaining intervals in the res
+        int i=0;
+        List<int[]> res=new ArrayList<>();
+        while(i<intervals.length &&newInterval[0]>intervals[i][1]){
             res.add(intervals[i]);
+            i=i+1;
+        }
+        // correct position
+        // intervals[i][0]-s1
+        // intervals[i][1]-e1
+        // newinterval[0]-s2
+        // newinterval[1]-e2
+        // intervals[i][1]>=newinterval[0] && newinterval[1]>=intervals[i][0]
+        // 
+        while(i<intervals.length && intervals[i][1]>=newInterval[0] && newInterval[1]>=intervals[i][0]){
+            // update new interval
+            newInterval[0]=Math.min( newInterval[0],intervals[i][0]);
+            newInterval[1]=Math.max( newInterval[1],intervals[i][1]);
             i++;
         }
-
-        // 2. Merge overlapping intervals
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-
-            newInterval[0] =
-                Math.min(newInterval[0], intervals[i][0]);
-
-            newInterval[1] =
-                Math.max(newInterval[1], intervals[i][1]);
-
-            i++;
-        }
-
-        // Add merged newInterval
         res.add(newInterval);
-
-        // 3. Remaining intervals
-        while (i < n) {
+        while(i<intervals.length){
             res.add(intervals[i]);
             i++;
         }
+        // res->output
+        int[][] ans=new int[res.size()][2];
 
-        return res.toArray(new int[res.size()][]);
+        for(int j=0;j<res.size();j++){
+            ans[j]=res.get(j);
+        }
+        return ans;
     }
 }
